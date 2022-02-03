aeb88ed248239c2c7758dfe12b57ab2ddb89cc3d
PedrazaDiego <skapet01@gmail.com>
2022-02-03 11:32:24 -0500

    routes and hamo page render

---
+++ client/src/pages/MyTrip.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>this is MyTrip</h1>
+    </div> 
+</template>
+
+
+
+
+<script>
+export default {
+    name: 'MyTrip',
+    components: {},
+    data: () => ({
+
+    }),
+    mounted(){
+
+    },
+    methods: {
+
+    }
+}
+</script>
