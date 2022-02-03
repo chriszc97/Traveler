e7ce43e37da63eec1cc0027411cb624bef8ea158
“Gihae <designbygihae@gmail.com>
2022-02-03 11:32:24 -0500

    update

--- traveler/admin.py
+++ traveler/admin.py
@@ -1,3 +1,10 @@
+
+# Register your models here.
 from django.contrib import admin
 
 # Register your models here.
+from .models import Country
+from .models import Location
+
+admin.site.register(Country)
+admin.site.register(Location)
