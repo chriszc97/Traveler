747908ef9e7cd3a51aef7a15bcdbb5d57efa79af
“Gihae <designbygihae@gmail.com>
2022-02-03 10:29:18 -0500

    update

--- traveler/models.py
+++ traveler/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    photo_url = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+     country= models.ForeignKey(Country, on_delete=models.CASCADE, related_name='destinatoin')
+     name = models.CharField(max_length=100)
+     photo_url = models.TextField(blank=True)
+     description= models.TextField()
+     food= models.CharField(max_length=100)
+     landmarks = models.CharField(max_length=100)
+     cost = models.CharField(max_length=100)
+
+     def __str__(self):
+        return self.name
