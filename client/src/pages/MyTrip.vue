89544ab05ebbfdda44397a97e5281e4611e849f8
PedrazaDiego <skapet01@gmail.com>
2022-02-04 09:13:25 -0500

    test cors

--- client/src/pages/MyTrip.vue
+++ client/src/pages/MyTrip.vue
@@ -1,131 +1,143 @@
 <template>
-    <div>
-        <h1>My new trips</h1>
-        <div v-if="!updated">
-            <form  @submit="handleSubmit">
-                <label> Country </label>
-                <input type="text"
-                name = 'name'
-                :value="name"
-                placeholder="Country to remember"
-                @input="handleCountry"
-                /> <br>
+  <div>
+    <h1>My new trips</h1>
+    <div v-if="!updated">
+      <form @submit="handleSubmit">
+        <label> Country </label>
+        <input
+          type="text"
+          name="name"
+          :value="name"
+          placeholder="Country to remember"
+          @input="handleCountry"
+        />
+        <br />
 
-                <label> Photo </label>
-                <input type="text"
-                name = 'photo_url'
-                :value="photo_url"
-                placeholder="Photo Url"
-                @input="handleCountry"
-                /> <br>
-                <button>Submit</button>
-            </form>
-        </div>
+        <label> Photo </label>
+        <input
+          type="text"
+          name="photo_url"
+          :value="photo_url"
+          placeholder="Photo Url"
+          @input="handleCountry"
+        />
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
 
-        <div v-if="!updated">
-            <form @submit="handleSubmitC">
-                <label>Name</label> 
-                <input type="text"
-                name='name'
-                :value="name"
-                placeholder="Destination's name"
-                @input="handleChange"
-                /> <br>
+    <div v-if="updated">
+      <form @submit="handleSubmitC">
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          :value="name"
+          placeholder="Destination's name"
+          @input="handleChange"
+        />
+        <br />
 
-                <label>Food</label>
-                <input type="text"
-                name='food'
-                :value="food"
-                placeholder="Food to remember"
-                @input="handleChange"
-                /> <br>
+        <label>Food</label>
+        <input
+          type="text"
+          name="food"
+          :value="food"
+          placeholder="Food to remember"
+          @input="handleChange"
+        />
+        <br />
 
-                <label>Photo</label>
-                <input type="text"
-                name='photo_url'
-                :value="photo_url"
-                placeholder="Photo url"
-                @input="handleChange"
-                /> <br>
+        <label>Photo</label>
+        <input
+          type="text"
+          name="photo_url"
+          :value="photo_url"
+          placeholder="Photo url"
+          @input="handleChange"
+        />
+        <br />
 
-                <label>Description</label>
-                <textarea
-                name='description'
-                :value='description'
-                @input="handleChange"
-                /> <br>
+        <label>Description</label>
+        <textarea
+          name="description"
+          :value="description"
+          @input="handleChange"
+        />
+        <br />
 
-                <label>Landmarks</label>
-                <input type="text"
-                name='landmarks'
-                :value="landmarks"
-                placeholder="Landmarks to remember"
-                @input="handleChange"
-                /> <br>
+        <label>Landmarks</label>
+        <input
+          type="text"
+          name="landmarks"
+          :value="landmarks"
+          placeholder="Landmarks to remember"
+          @input="handleChange"
+        />
+        <br />
 
-                <label>Cost</label>
-                <input type="text"
-                name='cost'
-                :value="cost"
-                placeholder="How much did I spent"
-                @input="handleChange"
-                /> <br>
+        <label>Cost</label>
+        <input
+          type="text"
+          name="cost"
+          :value="cost"
+          placeholder="How much did I spent"
+          @input="handleChange"
+        />
+        <br />
 
-                <button>Submit</button>
-            </form>
-        </div>
-    </div> 
+        <button>Submit</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 
 
 
 <script>
-import axios from 'axios'
-const BASE_URL = 'http://localhost:8000'
+import axios from "axios";
+const BASE_URL = "http://localhost:8000";
 export default {
-    name: 'MyTrip',
-    props: {
+  name: "MyTrip",
+  props: {},
+  components: {},
+  data: () => ({
+    updated: false,
+    country: {
+      name: "",
+      photo_url: "",
     },
-    components: {
-
+    destination: {
+      name: "",
+      food: "",
+      photo_url: "",
+      description: "",
+      landmarks: "",
+      cost: null,
+      country: 1,
     },
-    data: () => ({
-        updated: false,
-        country: {
-            name: '',
-            photo_url: '',
-        },
-        destination: {
-            name: '',
-            food: '',
-            photo_url: '',
-            description: '',
-            landmarks: '',
-            cost: null,
-            country: 1
-        }
-    }),
-    methods: {
-        async handleSubmit(e,country){
-            e.preventDefault()
-            this.updated = true
-            await axios.post(`${BASE_URL}/countries/`, {
-                country
-            })
-        },
-        handleChange(e){
-            this.destination[e.target.name] = e.target.value
-        },
-        handleCountry(e){
-            this.country[e.target.name] = e.target.value
-        },
-        async handleSubmitC(e, destination){
-            e.preventDefault()
-            await axios.post(`${BASE_URL}/destinations/`, {
-                destination
-            })
-        },
-    }
-}
+  }),
+  methods: {
+    async handleSubmit(e, country) {
+      e.preventDefault();
+      this.updated = true;
+      await axios.post(`${BASE_URL}/countries/`, {
+        country,
+      });
+    },
+    handleChange(e) {
+      this.destination[e.target.name] = e.target.value;
+    },
+    handleCountry(e) {
+      this.country[e.target.name] = e.target.value;
+    },
+    async handleSubmitC(e, destination) {
+      e.preventDefault();
+      await axios.post(`${BASE_URL}/destinations/`, {
+        destination,
+      });
+    },
+  },
+};
 </script>
--- traveler_django/settings.py
+++ traveler_django/settings.py
@@ -43,16 +43,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    
+
+
 ]
 
 ROOT_URLCONF = 'traveler_django.urls'
